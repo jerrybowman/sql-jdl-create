86a8df94b143929e85e8d00179eea77e5aa12b4c
Jerry <jerry.bowman.contractor@alsac.stjude.org>
2022-08-03 13:56:38 -0500

    Remove old command line code

--- src/main/java/com/jerrybowman/jhipster/jdl/SqlJdlCreateApplication.java
+++ src/main/java/com/jerrybowman/jhipster/jdl/SqlJdlCreateApplication.java
@@ -12,26 +12,11 @@ import java.util.Arrays;
 @SpringBootApplication
 public class SqlJdlCreateApplication {
 
-    private static Logger LOG = LoggerFactory.getLogger(SqlJdlCreateApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SqlJdlCreateApplication.class);
 
     public static void main(String[] args) {
         LOG.info("STARTING THE APPLICATION");
         SpringApplication.run(SqlJdlCreateApplication.class, args);
         LOG.info("APPLICATION FINISHED");
-    }
-
-//    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
     }
 }
